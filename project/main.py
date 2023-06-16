57e2645f8d179b2cce534bd7ca3f92dae19bb19a
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2023-06-16 15:56:34 -0600

    Added comments and player turns

--- project/main.py
+++ project/main.py
@@ -33,23 +33,39 @@ move_img = r"project\images\possible move.png"
 SPRITE_SCALING_PIECE = .5
 
 class Piece(arcade.Sprite):
+    # as is, Piece is just a class of arcade.sprite. As we add new attributes and methods, this would grow.
+
+    #This init is just default init for arcade.sprite
     def __init__(self, filename: str = None, scale: float = 1, image_x: float = 0, image_y: float = 0, image_width: float = 0, image_height: float = 0, center_x: float = 0, center_y: float = 0, repeat_count_x: int = 1, repeat_count_y: int = 1, flipped_horizontally: bool = False, flipped_vertically: bool = False, flipped_diagonally: bool = False, hit_box_algorithm: str | None = "Simple", hit_box_detail: float = 4.5, texture: Texture = None, angle: float = 0):
         super().__init__(filename, scale, image_x, image_y, image_width, image_height, center_x, center_y, repeat_count_x, repeat_count_y, flipped_horizontally, flipped_vertically, flipped_diagonally, hit_box_algorithm, hit_box_detail, texture, angle)
 
+
+    #This was an attempt for implementing the movement of pieces.
     # def update(self):
     #     self.center_x = self.change_x
     #     self.center_y = self.change_y
 
 
+#the main window for checkers and what maintains the gamestate.
 class Checkers(arcade.Window):
     
-
+    #default parameters for arcade.window.
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
+        #set the background color
         self.background_color = BACKGROUND
 
+
+        #this is to keep track of the last clicked piece.
         self.piece_index = None
+
+        #track player's turns
+        self.is_p1_turn = True
+
+        #initialize all the sprites for the board, all the pieces, and possible moves. 
+        #the first list is the default sprite list
+        #the second tracks the sprites in 2 dimensions.
 
 
         #sprite list for board
@@ -69,130 +85,226 @@ class Checkers(arcade.Window):
         self.possible_move_grid = []
         
         #setup board
+
+        #Two for loops to make a 2d space
         for row in range(ROW_COUNT):
+
+            #this will alternate the starting condition for each row so that things can be checkered.
             if row % 2 == 0:
                 checker_checker = 0
             else:
                 checker_checker = 1
+            
+            #we append a new list for a column, each item containing an item at a coordinate. The coordinate is [row][column]
             self.grid_sprites.append([])
             self.p1_grid.append([])
             self.p2_grid.append([])
             self.possible_move_grid.append([])
+
+            #filling the content of each coordinate.
             for column in range(COLUMN_COUNT):
+
+                #convert x and y to an 8 x 8 grid metric
                 x = column * (CELL_WIDTH + CELL_MARGIN) + (CELL_WIDTH / 2 + CELL_MARGIN)
                 y = row * (CELL_HEIGHT + CELL_MARGIN)  + (CELL_HEIGHT /2 + CELL_MARGIN)
+
+                #Will alternate due to the checkered pattern of the game.
                 if checker_checker == 0:
+                    # create the board tile
                     sprite = arcade.SpriteSolidColor(CELL_WIDTH, CELL_HEIGHT, arcade.color.TAN)
+                    # alternate the checker
                     checker_checker = 1
+
+                    # Make the possible move sprite
+
+                    # create a possible move sprite at each location.
                     possible_move = arcade.Sprite(move_img, SPRITE_SCALING_PIECE)
+                    # Give it its center at this location.
                     possible_move.center_x = x
                     possible_move.center_y = y
+                    # set its visibility to false by default. 
                     possible_move.visible = False
+                    # append the possible move to the list and to this coordinate.
                     self.possible_move_list.append(possible_move)
                     self.possible_move_grid[row].append(possible_move)
+
+                    #If we are within the starting bounds of player 1.
                     if row <= 2:
+                        #Make the player 1 piece
                         p1_piece = Piece(p1_img, SPRITE_SCALING_PIECE)
+                        #set its center
                         p1_piece.center_x = x
                         p1_piece.center_y = y
+                        #append it to the list and the coordinate
                         self.p1_list.append(p1_piece)
                         self.p1_grid[row].append(p1_piece)
-                        self.p2_grid[row].append("empty")
-                    elif row >= 5:
-                        
-                        p2_piece = Piece(p2_img, SPRITE_SCALING_PIECE)
-                        p2_piece.center_y = y
-                        p2_piece.center_x = x
-                        self.p2_list.append(p2_piece)
-                        self.p2_grid[row].append(p2_piece)
-                        self.p1_grid[row].append("empty")
-                    else:
-                        self.p1_grid[row].append("empty")
+                        #set player2 empty at this location to maintain a proper grid.
                         self.p2_grid[row].append("empty")
 
+                    #if we are within the starting bounds of player 2.
+                    elif row >= 5:
+                        #make the player 2 piece
+                        p2_piece = Piece(p2_img, SPRITE_SCALING_PIECE)
+                        #set its center
+                        p2_piece.center_y = y
+                        p2_piece.center_x = x
+                        #append to list and coordinates
+                        self.p2_list.append(p2_piece)
+                        self.p2_grid[row].append(p2_piece)
+                        #and set player 1 empty at this location to maintain the grid.
+                        self.p1_grid[row].append("empty")
+                    else:
+                        #we are within the area between starting positions so set both to empty to maintain the grid.
+                        self.p1_grid[row].append("empty")
+                        self.p2_grid[row].append("empty")
+                #If we are on the unused half of the checkered board.
                 else:
+                    # set the board sprite
                     sprite = arcade.SpriteSolidColor(CELL_WIDTH, CELL_HEIGHT, arcade.color.RED_BROWN)
+                    #alternate the checker
                     checker_checker = 0
+                    #and append empties to maintain the grid.
                     self.p1_grid[row].append("empty")
                     self.p2_grid[row].append("empty")
                     self.possible_move_grid[row].append("empty")
+                #finally make the board sprite depending on color
                 sprite.center_x = x
                 sprite.center_y = y
                 self.grid_sprite_list.append(sprite)
                 self.grid_sprites[row].append(sprite)
     
-    def update_piece_position(self, piece):
-        self.p1_list
+    #an attempt to make player movement functional
+    # def update_piece_position(self, piece):
+    #     self.p1_list
 
+
+    #the main method that is called to render all sprites in the window
     def on_draw(self):
 
+        #always reset
         self.clear()
+        #draw the board
         self.grid_sprite_list.draw()
+        #draw the player 1 pieces
         self.p1_list.draw()
+        #draw the player 2 pieces
         self.p2_list.draw()
+        #draw the possible moves (though by default they are not visible.)
         self.possible_move_list.draw()
 
+    #whenever the player clicks, this will dictate what happens. parameters are the default as defined by arcade.
     def on_mouse_press(self, x, y, button, modifiers):
+
+        #Convert the mouse click position to an 8x8 grid
         column = int(x // (CELL_WIDTH + CELL_MARGIN))
         row = int(y // (CELL_HEIGHT + CELL_MARGIN))
-        if self.piece_index != None:
-            if self.possible_move_grid[row][column] != "empty":
-                if self.possible_move_grid[row][column].visible == True:
-                    move_to_index = find_nearest_piece(self.possible_move_list, column, row)
-                    if self.possible_move_list.sprite_list[move_to_index].center_x > self.p1_list.sprite_list[self.piece_index].center_x:
-                        self.p1_grid[row][column] = self.p1_grid[row - 1][column - 1]
-                        self.p1_grid[row - 1][column - 1] = "empty"
-                    else:
-                        self.p1_grid[row][column] = self.p1_grid[row - 1][column + 1] 
-                        self.p1_grid[row - 1][column + 1] = "empty"
 
-                    self.p1_list.sprite_list[self.piece_index].center_x = self.possible_move_list.sprite_list[move_to_index].center_x
-                    self.p1_list.sprite_list[self.piece_index].center_y = self.possible_move_list.sprite_list[move_to_index].center_y
-                    
-                    self.piece_index = None
-                    print("change location")
-                    for i in self.possible_move_list.sprite_list:
-                        i.visible = False
-        else:        
+        #if we have already clicked a piece before this current click.
+        if self.piece_index != None:
+            #and we clicked a location where there is a possible move entity
+            if self.possible_move_grid[row][column] != "empty":
+                #and if that entity is visible
+                if self.possible_move_grid[row][column].visible == True:
+                    #find which square the player clicked on
+                    move_to_index = find_nearest_piece(self.possible_move_list, column, row)
+                    #if it's player 1 turn
+                    if self.is_p1_turn:
+                        #if the player clicked on the left one
+                        if self.possible_move_list.sprite_list[move_to_index].center_x > self.p1_list.sprite_list[self.piece_index].center_x:
+                            # move the piece to the clicked location
+                            self.p1_grid[row][column] = self.p1_grid[row - 1][column - 1]
+                            # and clear where the piece used to be.
+                            self.p1_grid[row - 1][column - 1] = "empty"
+                        #else, the player clicked on the right one
+                        else:
+                            #Move the piece to the clicked position
+                            self.p1_grid[row][column] = self.p1_grid[row - 1][column + 1] 
+                            #and clear where the piece used to be.
+                            self.p1_grid[row - 1][column + 1] = "empty"
+
+                        #update the new centers for the piece.
+                        self.p1_list.sprite_list[self.piece_index].center_x = self.possible_move_list.sprite_list[move_to_index].center_x
+                        self.p1_list.sprite_list[self.piece_index].center_y = self.possible_move_list.sprite_list[move_to_index].center_y
+                        #switch player turn.
+                        self.is_p1_turn = False
+                    else:
+                        #if the player clicked on the left one
+                        if self.possible_move_list.sprite_list[move_to_index].center_x > self.p1_list.sprite_list[self.piece_index].center_x:
+                            # move the piece to the clicked location
+                            self.p2_grid[row][column] = self.p2_grid[row + 1][column - 1]
+                            # and clear where the piece used to be.
+                            self.p1_grid[row + 1][column - 1] = "empty"
+                        #else, the player clicked on the right one
+                        else:
+                            #Move the piece to the clicked position
+                            self.p2_grid[row][column] = self.p2_grid[row + 1][column + 1] 
+                            #and clear where the piece used to be.
+                            self.p2_grid[row + 1][column + 1] = "empty"
+
+                        #update the new centers for the piece.
+                        self.p2_list.sprite_list[self.piece_index].center_x = self.possible_move_list.sprite_list[move_to_index].center_x
+                        self.p2_list.sprite_list[self.piece_index].center_y = self.possible_move_list.sprite_list[move_to_index].center_y
+                        #Switch the player's turn
+                        self.is_p1_turn = True
+            #clear the clicked index
+            self.piece_index = None
+            #print statement for testing
+            print("change location")
+            #clear all showing possible moves
             for i in self.possible_move_list.sprite_list:
                 i.visible = False
+        #if we have not yet clicked on a piece
+        else:
 
             
-            
+            #Theres a chance a player clicked without the bounds of the game, if so just ignore the click.
             if row >=  ROW_COUNT or column >= COLUMN_COUNT:
                 return
             
-            if self.p1_grid[row][column] != "empty":
+            #if there is a player 1 piece at the clicked location and it is player 1's turn
+            if self.p1_grid[row][column] != "empty" and self.is_p1_turn:
+                #print for testing
                 print(f"Clicked on a player 1 piece. at {row},{column}")
+                #find the index of that piece and set it to the variable that tracks if a piece has been clicked
                 self.piece_index = find_nearest_piece(self.p1_list.sprite_list, column, row)
                 
-                
+                #reveal the usual two possible moves
                 self.possible_move_grid[row + 1][column + 1].visible = True
                 self.possible_move_grid[row + 1][column - 1].visible = True
-                
-            elif self.p2_grid[row][column] != "empty":
+            
+            #if there is a player 2 piece at the clicked location and it's player 2's turn
+            elif self.p2_grid[row][column] != "empty" and self.is_p1_turn == False:
+                #print for testing
                 print(f"Clicked on a player 2 piece. at {row},{column}")
-                index = find_nearest_piece(self.p2_list.sprite_list, column, row)
-                self.p2_list.sprite_list[index].center_x -= (CELL_WIDTH + CELL_MARGIN)
-                self.p2_list.sprite_list[index].center_y -= (CELL_HEIGHT + CELL_MARGIN)
-                self.p2_grid[row - 1][column - 1] = self.p2_grid[row][column]
-                self.p2_grid[row][column] = "empty"
+                #find the index of that piece and set it to the variable that tracks if a piece has been clicked
+                self.piece_index = find_nearest_piece(self.p2_list.sprite_list, column, row)
+                
+                #reveal the usual two possible moves
+                self.possible_move_grid[row - 1][column + 1].visible = True
+                self.possible_move_grid[row - 1][column - 1].visible = True
         
+    #update the lists. this just keeps things flowing nice.
     def on_update(self, delta_time):
         self.p1_list.update()
         self.p2_list.update()
         self.possible_move_list.update()
 
-
+# function to find the index of the piece nearest to where the click occurred. 
 def find_nearest_piece(sprite_list, column, row):
+    #excessively large number
     closest_index_x = 10000
     closest_index_y = 10000
+    #for each index in the length of the list
     for i in range(len(sprite_list)):
+        #check to see if we are still within the bounds so we can properly check the next one.
         if i < len(sprite_list):
+            #if x or y is closer to where it was clicked
             if (abs((sprite_list[i].center_x // (CELL_WIDTH + CELL_MARGIN)) - column) < closest_index_x) or (abs((sprite_list[i].center_y // (CELL_HEIGHT + CELL_MARGIN)) - row) < closest_index_y) :
+                # Set the current closest to that index
                 closest_index_x = abs((sprite_list[i].center_x // (CELL_WIDTH + CELL_MARGIN)) - column)
                 closest_index_y = abs((sprite_list[i].center_y // (CELL_HEIGHT + CELL_MARGIN))- row)
                 closest_index = i
-    
+    # return the index of the sprite that was closest to the clicked location.
     return closest_index
 
 
